0dab04efd129c022e8fed5b6e6fd1a0dbc516ea7
Charly2 <papapitufo10@gmail.com>
2018-03-10 12:16:38 -0600

    cambios

--- routes/index.js
+++ routes/index.js
@@ -12,14 +12,19 @@ var reporteEmpresa = require('../models/Reporte_EmpresaModel');7
 var comentarioModel = require('../models/Comentario');
 var Notas = require('../models/NotaModel');
 var Archivos = require('../models/Archivo_Mensaje');
+var fs = require('file-system');
 /* GET home page. */
 
-function include_js(file){
-    eval(fs.readFileSync(file+'.js')+'');
-}
-
-include_js("actions");
-
+router.get('/report-submit', function(req, res, next) {
+  auth.mach(req,res,function (req,res) {
+    reporteLcal.getAll(function (err, data) {
+      if (!err){
+        //Recibe datos del wizard
+        res.render('report_history',{title: 'Sicty report system',datos:data,user:req.session.nombre});
+      }
+    });
+  });
+});
 
 router.get('/', function(req, res, next) {
 
