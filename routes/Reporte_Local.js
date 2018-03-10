08545625c27d0ba92725c3794cdb5e1d573c4d9b
Charly2 <papapitufo10@gmail.com>
2018-03-10 17:24:34 -0600

    datos Local

--- routes/Reporte_Local.js
+++ routes/Reporte_Local.js
@@ -24,6 +24,7 @@ router.get('/:id', function(req, res, next) {
         reporteLcal.findById(id,function (err, data) {
             if (!err){
                 datos.info = data[0];
+                data.info.propiedades = JSON.stringify(data.info.propiedades);
                 comentarioModel.clientefindByReporte(id,function (errC,dataC) {
                     if(!errC){
                         datos.comentariosC = dataC;
