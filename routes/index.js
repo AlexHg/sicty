d058c9268e05898868558d2c4bcd92b49ee7612f
Alejandro Hernandez <alexxh42@gmail.com>
2018-03-10 13:58:32 -0600

    category_list

--- routes/index.js
+++ routes/index.js
@@ -73,9 +73,27 @@ router.get('/report-submit', function(req, res, next) {
     });
 });
 
-router.get('/new-category', function(req, res, next) {
 
-    res.render('new_category', { title: 'Sicty report system',user:req.session.nombre });
+router.get('/new-category', function(req, res, next) {
+    auth.mach(req,res,function (req,res) {
+        reporteLcal.getAll(function (err, data) {
+            if (!err){
+                //Recibe datos del wizard
+                res.render('new_category',{title: 'Sicty report system',datos:data,user:req.session.nombre});
+            }
+        });
+    });
+});
+
+router.get('/category-list', function(req, res, next) {
+    auth.mach(req,res,function (req,res) {
+        reporteLcal.getAll(function (err, data) {
+            if (!err){
+                //Recibe datos del wizard
+                res.render('category_list',{title: 'Sicty report system',datos:data,user:req.session.nombre});
+            }
+        });
+    });
 });
 
 router.get('/new-report', function(req, res, next) {
